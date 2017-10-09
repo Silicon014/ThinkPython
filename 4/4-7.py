977df58ae3cf6a890dcef64795d6be3c00a3813c
Silicon014 <kaguya.h@foxmail.com>
2017-10-09 20:59:44 +0800

    Upload

---
+++ 4/4-7.py
@@ -0,0 +1,17 @@
+import turtle
+import math
+def polyline(t, n, length, angle):
+	for i in range(n):
+		t.fd(length)
+		t.lt(angle)
+def arc(t, r, angle):
+	arc_length = math.pi * r * angle / 180
+	n = int(arc_length / 3) + 1
+	step_length = arc_length / n
+	step_angle = angle / n
+	polyline(t, n, step_length, step_angle)
+def polygon(t, n, length):
+	polyline(t, n, length, 360 / n)
+def circle(t, r):
+	arc(t, r, 360)
+
